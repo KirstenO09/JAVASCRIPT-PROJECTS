61abf0dfd7cf17fe3209c5ed8c47508e15410fb6
KirstenO09 <hf3984@wayne.edu>
2025-07-17 19:49:36 -0400

    added more changes to JavaScript project
    Specifically added comments and breaks to the code

--- BasicJavaScriptProjects/Project1_expressions_alert/JS/main.js
+++ BasicJavaScriptProjects/Project1_expressions_alert/JS/main.js
@@ -1,6 +1,6 @@
-var Sent1 = "This is the beginning of the string";
-var Sent2 = "and this is the end of the string";
-var Sent3 = Sent1 + " " + Sent2;
-var expression = 3 + 3;
+var Sent1 = "This is the beginning of the string"; // This is the first part of the string
+var Sent2 = "and this is the end of the string"; // This is the second part of the string
+var Sent3 = Sent1 + " " + Sent2; // Concatenating the two strings
+var expression = 3 + 3; // This is a simple arithmetic expression
 
 
