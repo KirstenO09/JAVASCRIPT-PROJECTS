98519db50f8ff473bc4bccb7f50e5f01c7144cd1
KirstenO09 <hf3984@wayne.edu>
2025-07-18 20:42:35 -0400

    Added comments to Project4_dictionaries files
    Added comments to the HTML and JS files in the Project4_dictionaries

--- BasicJavaScriptProjects/Project4_dictionaries/JS/main.js
+++ BasicJavaScriptProjects/Project4_dictionaries/JS/main.js
@@ -6,7 +6,9 @@ function dictionary(){
         Year:"2022",
         Doors: 4,
         Engine:"Hybrid",
-    };
-    delete Vehicle.Doors;
-    document.getElementById("dictionary").innerHTML = Vehicle.Doors; //
+    }; //This is a dictionary that contains key-value pairs (KVPs)
+    delete Vehicle.Doors; //This uses the delete operator to remove the value (4) of the key (Doors) from the dicionary
+    document.getElementById("dictionary").innerHTML = Vehicle.Doors; 
+    // This is suppose to display the value (4) of the Doors key in an HTML element with the ID "dictionary".
+    //However, since it used the delete operator it will only display undefined.
 }
